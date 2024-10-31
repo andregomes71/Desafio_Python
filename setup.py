67613e69085c08f139f3aa1a45ee1b7e630906f4
Andre Gomes - DevOps Engineer <agomes@humangov.com>
2024-10-31 11:19:21 -0300

    Pacote Image Processing

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image-processing-package",
+    description="Criação de pacotes de processamento de imagens em Python",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
